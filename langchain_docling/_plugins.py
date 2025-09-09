a55d08cab417f522e5ebf5e7424d0a0b02c87e08
Michele Dolfi <dol@zurich.ibm.com>
2025-09-09 13:11:44 +0200

    add docstrings
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- langchain_docling/_plugins.py
+++ langchain_docling/_plugins.py
@@ -1,4 +1,8 @@
+"""Register Docling plugins."""
+
+
 def picture_description():
+    """Picture description plugins."""
     from langchain_docling.picture_description import PictureDescriptionLangChainModel
 
     return {
--- langchain_docling/picture_description.py
+++ langchain_docling/picture_description.py
@@ -1,35 +1,36 @@
+"""Picture description model using LangChain primitives."""
+
 import base64
-from collections.abc import Iterable
 import io
+from collections.abc import Iterable
 from pathlib import Path
 from typing import ClassVar, Literal, Optional, Type, Union
 
+from docling.datamodel.accelerator_options import AcceleratorOptions
+from docling.datamodel.pipeline_options import PictureDescriptionBaseOptions
+from docling.models.picture_description_base_model import PictureDescriptionBaseModel
+from docling.models.utils.hf_model_download import HuggingFaceModelDownloadMixin
+from langchain_core.language_models.chat_models import BaseChatModel
 from PIL import Image
 
-from docling.datamodel.accelerator_options import AcceleratorOptions
-from docling.datamodel.pipeline_options import (
-    PictureDescriptionBaseOptions,
-)
-from docling.models.picture_description_base_model import PictureDescriptionBaseModel
-from docling.models.utils.hf_model_download import (
-    HuggingFaceModelDownloadMixin,
-)
-
-from langchain_core.language_models.chat_models import BaseChatModel
 
 class PictureDescriptionLangChainOptions(PictureDescriptionBaseOptions):
+    """Options for the PictureDescriptionLangChainModel."""
+
     kind: ClassVar[Literal["langchain"]] = "langchain"
     llm: BaseChatModel
     prompt: str = "Describe this document picture in a few sentences."
     provenance: Optional[str] = None
 
 
-
 class PictureDescriptionLangChainModel(
     PictureDescriptionBaseModel, HuggingFaceModelDownloadMixin
 ):
+    """Implementation of a PictureDescription model using LangChain."""
+
     @classmethod
     def get_options_type(cls) -> Type[PictureDescriptionBaseOptions]:
+        """Define the option type for the factory."""
         return PictureDescriptionLangChainOptions
 
     def __init__(
@@ -40,6 +41,7 @@ class PictureDescriptionLangChainModel(
         options: PictureDescriptionLangChainOptions,
         accelerator_options: AcceleratorOptions,
     ):
+        """Initialize PictureDescriptionLangChainModel."""
         super().__init__(
             enabled=enabled,
             enable_remote_services=enable_remote_services,
@@ -56,7 +58,7 @@ class PictureDescriptionLangChainModel(
                 self.provenance += f"-{self.options.provenance}"
 
     def _annotate_images(self, images: Iterable[Image.Image]) -> Iterable[str]:
-
+        """Annotate the images with the LangChain model."""
         # Create input messages
         batch_messages = []
 
@@ -66,20 +68,23 @@ class PictureDescriptionLangChainModel(
             buffered = io.BytesIO()
             image.save(buffered, format="PNG")
             image_data = base64.b64encode(buffered.getvalue()).decode("utf-8")
-            batch_messages.append([
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": self.options.prompt},
+            batch_messages.append(
+                [
                     {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/png;base64,{image_data}"},
-                    },
-                ],
-            }]
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text": self.options.prompt},
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/png;base64,{image_data}"
+                                },
+                            },
+                        ],
+                    }
+                ]
             )
 
         responses = self.llm.batch(batch_messages)
         for resp in responses:
             yield resp.text()
-
